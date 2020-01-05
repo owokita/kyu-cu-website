5c239b8600f8f05a1bfba1e940c8b8a258218279
dijiflex <dijiflex@gmail.com>
2020-01-05 14:49:42 +0300

    deleted croppies js

--- user/settings.php
+++ user/settings.php
@@ -168,7 +168,7 @@
 <script src="js/lib/popper.min.js"></script>
 <script src="js/lib/bootstrap.min.js"></script>
 <script src="js/lib/admin.js"></script>
-<script src="js/lib/croppie.min.js"></script>
+
 <!-- Side Navigation Scripts -->
 <script>
     $(document).ready(function() {
@@ -208,68 +208,7 @@
 
         });
 </script>
-<script>
-    $(document).ready(function() {
 
-        $image_crop = $('#image_demo').croppie({
-            enableExif: true,
-            viewport: {
-                width: 200,
-                height: 200,
-                type: 'circle' //circle
-            },
-            boundary: {
-                width: 300,
-                height: 300
-            }
-        });
-
-        $('#insert_image').on('change', function() {
-            var reader = new FileReader();
-            reader.onload = function(event) {
-                $image_crop.croppie('bind', {
-                    url: event.target.result
-                }).then(function() {
-                    console.log('jQuery bind complete');
-                });
-            }
-            reader.readAsDataURL(this.files[0]);
-            $('#insertimageModal').modal('show');
-        });
-
-        $('.crop_image').click(function(event) {
-            $image_crop.croppie('result', {
-                type: 'canvas',
-                size: 'viewport'
-            }).then(function(response) {
-                $.ajax({
-                    url: 'includes/insertfile.php',
-                    type: 'POST',
-                    data: {
-                        "image": response
-                    },
-                    success: function(data) {
-                        $('#insertimageModal').modal('hide');
-                        load_images();
-                        alert(data);
-                    }
-                })
-            });
-        });
-
-        load_images();
-
-        function load_images() {
-            $.ajax({
-                url: "fetch_images.php",
-                success: function(data) {
-                    $('#store_image').html(data);
-                }
-            })
-        }
-
-    });
-</script>
 
 
 
