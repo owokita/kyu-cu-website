65535fe5011bb4af67b30cda67a4a49b291a2888
owokita <60746019+owokita@users.noreply.github.com>
2020-02-06 19:01:04 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,18 +1,1 @@
-THIS IS FELIX
-
-THIS IS THE SECOND CHANGE
-
-<?php
-
-?php
-this
-                                    $artOBJ = new  ARTICLE();
-                                    $comments = $artOBJ->getcomments($article['article_id']);
-                                    foreach ($comments as $comment):
-                                     ?>
-                                    <div class="col  ml-5 mt-1 flex-fill text-right border rounded">
-                                        <?php  echo $comment['comment'] ?>
-                                        <br><span class="font-italic font-weight-bold" style="font-size: 0.7em"><?php  echo $comment['user_fname']; echo " "; echo $comment['user_lname'];  ?></span>
-                                    </div>
-                                    <?php endforeach ?>
-                                    
+akljfldksjflkasdjfklasdjf aldfnlkasdfksdjflksdl
